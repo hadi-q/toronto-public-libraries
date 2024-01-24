2d9b3afe88bff95cd0e3386fc5837ef6a1956d18
Hadi Ahmad <hadiq.ahmad@mail.utoronto.ca>
2024-01-23 22:27:27 -0500

    Fixed whitespace issues

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -4,11 +4,12 @@
 # Date: 22 January 2024
 # Contact: hadiq.ahmad@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
+# Pre-requisites: Know where to get Toronto open data
 # Any other information needed? The 25 ward model was used for this script 
-#                               instead of the newer 44 ward model
+#                               instead of the older 44 ward model
 
 #### Workspace setup ####
+# install.packages("tidyverse")
 library(tidyverse)
 
 #### Simulate data ####
@@ -18,7 +19,15 @@ simulated_data <-
     "Branch" = 1:112,
     # Randomly pick an option, with replacement, 112 times
     "Ward" = sample(
-      x = c("Beaches-East York", "Davenport", "Don Valley East", "Don Valley North", "Don Valley West", "Eglinton-Lawrence", "Etobicoke Centre", "Etobicoke North", "Etobicoke-Lakeshore", "Humber River-Black Creek", "Parkdale-High Park", "Scarborough Centre", "Scarborough North", "Scarborough Southwest", "Scarborough-Agincourt", "Scarborough-Guildwood", "Scarborough-Rouge Park", "Spadina-Fort York", "Toronto Centre", "Toronto-Danforth", "Toronto-St. Paul's", "University-Rosedale", "Willowdale", "York Centre", "York South-Weston"),
+      x = c("Beaches-East York", "Davenport", "Don Valley East", 
+            "Don Valley North", "Don Valley West", "Eglinton-Lawrence", 
+            "Etobicoke Centre", "Etobicoke North", "Etobicoke-Lakeshore", 
+            "Humber River-Black Creek", "Parkdale-High Park", 
+            "Scarborough Centre", "Scarborough North", "Scarborough Southwest", 
+            "Scarborough-Agincourt", "Scarborough-Guildwood", 
+            "Scarborough-Rouge Park", "Spadina-Fort York", "Toronto Centre", 
+            "Toronto-Danforth", "Toronto-St. Paul's", "University-Rosedale", 
+            "Willowdale", "York Centre", "York South-Weston"),
       size = 112,
       replace = TRUE
     )
@@ -26,3 +35,23 @@ simulated_data <-
 
 simulated_data
 
+# Ensure correct column name selected from simulated data
+names(simulated_data)
+
+# Test cases
+
+# Ensure that each ward is randomly assigned >1 library branch
+simulated_data$Ward |>
+  unique() == c("Beaches-East York", "Davenport", "Don Valley East", 
+                "Don Valley North", "Don Valley West", "Eglinton-Lawrence", 
+                "Etobicoke Centre", "Etobicoke North", "Etobicoke-Lakeshore", 
+                "Humber River-Black Creek", "Parkdale-High Park", 
+                "Scarborough Centre", "Scarborough North", "Scarborough Southwest", 
+                "Scarborough-Agincourt", "Scarborough-Guildwood", 
+                "Scarborough-Rouge Park", "Spadina-Fort York", "Toronto Centre", 
+                "Toronto-Danforth", "Toronto-St. Paul's", "University-Rosedale", 
+                "Willowdale", "York Centre", "York South-Weston")
+# Ensure that 25 wards are represented
+simulated_data$Ward |>
+  unique() |>
+  length() == 25
