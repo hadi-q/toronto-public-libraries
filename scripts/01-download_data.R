24aa39fed5bcf7d0478f38c6e11a5096d33fa755
Hadi Ahmad <hadiq.ahmad@mail.utoronto.ca>
2024-01-23 22:18:35 -0500

    Added test code to retrieve data reproducibly from Toronto Open Data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,20 +1,46 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Hadi Ahmad
+# Date: 23 January 2023
+# Contact: hadiq.ahmad@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Know where to get Toronto open data
+# Any other information needed? The 25 ward model was used for this script 
+#                               instead of the older 44 ward model
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Code copied from https://open.toronto.ca/dataset/library-branch-general-information/
+# get package
+
+package <- show_package("f5aa9b07-da35-45e6-b31f-d6790eb9bd9b")
+
+package
+
+
+
+# get all resources for this package
+
+resources <- list_package_resources("f5aa9b07-da35-45e6-b31f-d6790eb9bd9b")
+
+
+
+# identify datastore resources; by default, Toronto Open Data sets datastore 
+# resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+
+
+# load the first datastore resource as a sample
+
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+data
 
 
 
